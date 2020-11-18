2d9a5444e133bcb89bfe4c8319213f7950552af6
Destiny Abellana <d.abellana@arcanys.com>
2020-11-18 14:11:33 +0800

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { StoreModule } from "@ngrx/store";
+import { reducers, metaReducers } from "./store";
+import { HydrationEffects } from "./store/hydration/hydration.effects";
+import { EffectsModule } from "@ngrx/effects";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, StoreModule.forRoot(reducers, { metaReducers }), EffectsModule.forRoot([HydrationEffects])],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
+/*
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://github.com/ngrx/platform
+*/
